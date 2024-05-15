bae63a9247626e0b382d9ae59894a03de5a05518
SvetlanaSil <svetasilchenkova@mail.ru>
2024-05-15 20:18:29 +0300

    Commit

--- app_weather/views.py
+++ app_weather/views.py
@@ -4,11 +4,13 @@ from weather_api import current_weather
 
 
 def current_weather_view(request):
-        lat = request.GET.get("lat")
-        lon = request.GET.get("lon")
-        if lat and lon:
-            data = current_weather(lat=lat, lon=lon)
-        else:
-            data = current_weather(55.75, 37.61)
+    if request.method == "GET":
+        data = current_weather(55.75, 37.61)
+    #     lat = request.GET.get("lat")
+    #     lon = request.GET.get("lon")
+    #     if lat and lon:
+    #         data = current_weather(lat=lat, lon=lon)
+    #     else:
+    #         data = current_weather(55.75, 37.61)
         return JsonResponse(data, safe=False, json_dumps_params={'ensure_ascii': False,
                                                      'indent': 4})
